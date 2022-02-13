ede99a0db891da92df0adcaa4b39cd2abc2f1d90
ali abozaid <ali.abozaid22@gmail.com>
2022-02-14 00:36:14 +0100

    remove unused navController

--- app/src/main/kotlin/io/aliabozid/justeat/MainActivity.kt
+++ app/src/main/kotlin/io/aliabozid/justeat/MainActivity.kt
@@ -2,22 +2,15 @@ package io.aliabozid.justeat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import io.aliabozid.justeat.assets.utils.viewBinding
 import io.aliabozid.justeat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::inflate)
-    private var navController: NavController? = null
+	private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::inflate)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        (supportFragmentManager.findFragmentById(R.id.container) as? NavHostFragment)?.let {
-            navController = it.navController
-        }
-        window.setBackgroundDrawableResource(R.color.background)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+		window.setBackgroundDrawableResource(R.color.background)
+	}
 }
