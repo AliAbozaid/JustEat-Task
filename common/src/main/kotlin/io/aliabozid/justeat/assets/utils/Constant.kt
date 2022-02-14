8c4594009786ba7bb297f80559ec561eaf60b95e
ali abozaid <ali.abozaid22@gmail.com>
2022-02-14 01:17:13 +0100

    Remove unused val from Constant

--- common/src/main/kotlin/io/aliabozid/justeat/assets/utils/Constant.kt
+++ common/src/main/kotlin/io/aliabozid/justeat/assets/utils/Constant.kt
@@ -1,11 +1,6 @@
 package io.aliabozid.justeat.assets.utils
 
 object Constant {
-    object Separator {
-        const val SPACE = " "
-        const val NEW_LINE = "\n"
-    }
-
     object Preference {
         const val SHARED_PREF_NAME = "just_eat_pref"
     }
